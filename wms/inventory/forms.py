cab3b588c43de38183920b8871b26b682dcf6327
cxmx <covxx@users.noreply.github.com>
2024-07-03 17:07:45 -0400

    Cleaned up nav bar and added more options

--- wms/inventory/forms.py
+++ wms/inventory/forms.py
@@ -13,4 +13,5 @@ class InventoryItemForm(forms.ModelForm):
     category = forms.ModelChoiceField(queryset=Category.objects.all(), initial=0) 
     class Meta:
         model = InventoryItem
-        fields = ['name', 'complaint', 'date_complained','built_by', 'date_built', 'category']    
+        fields = ['name', 'complaint', 'date_complained','built_by', 'date_built', 'category'] 
+    # Want to add a cost field and image uploading. Need to clean up the form view under item_form.html
