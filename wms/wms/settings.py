c670f9988e0e1dfcfc574fe1584958eba735b6f2
cxmx <covxx@users.noreply.github.com>
2024-07-05 00:33:17 -0400

    RC - forms cleaned up, all base features working in forms, no current erros.

--- wms/wms/settings.py
+++ wms/wms/settings.py
@@ -167,6 +167,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/user_images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
